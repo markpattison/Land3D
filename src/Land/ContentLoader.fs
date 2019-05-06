a2c197818a16a8be7b1d0ec3bf2a72892d87d76f
markpattison <mr.pattison@gmail.com>
2019-05-06 19:10:38 +0100

    remove dead code

--- src/Land/ContentLoader.fs
+++ src/Land/ContentLoader.fs
@@ -22,10 +22,6 @@ type Textures =
     }
 
 let loadEffects (game: Game) =
-    let a = game.Content.Load<Effect>("Effects/effects")
-    let b = game.Content.Load<Effect>("Effects/hdr")
-    let c = game.Content.Load<Effect>("Effects/skyFromAtmosphere")
-    let d = game.Content.Load<Effect>("Effects/groundFromAtmosphere")
     {
         Effect = game.Content.Load<Effect>("Effects/effects")
         Hdr = game.Content.Load<Effect>("Effects/hdr")
